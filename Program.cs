07341469ca08cf791b17b61d5c09d560d6a10ad6
MikhailYurkov <9906205@gmail.com>
2022-02-23 17:10:17 +0300

    add SearchArrayLengthMin3 and CreateArrayLengthMin3

--- Program.cs
+++ Program.cs
@@ -1,14 +1,20 @@
-﻿/* Задача: Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+﻿/* Задача: Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
+либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+лучше обойтись исключительно массивами.
 
 Примеры:
 
-["hello", "2", "world", ":-)"] -> ["2", ":-)"]
+["hello", "2", "world", ":-)"] -> ["2", ":-)"
 
 ["1234", "1567", "-2", "computer science"] -> ["-2"]
 
 ["Russia", "Denmark", "Kazan"] -> []
 */
 using static System.Console;
+
+string[] stringArray = { "Hello", "    ", "world", "2", ":-)", "1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan" };
+
 void PrintStringArr(string[] stringArray)
 {
     Write("[");
@@ -22,7 +28,8 @@ void PrintStringArr(string[] stringArray)
     }
     Write("]");
 }
-int CreateArrayLengthMin3 (string[] stringArray)
+
+int SearchArrayLengthMin3(string[] stringArray)
 {
     int numberOfString = 0;
     for (int i = 0; i < stringArray.Length; i++)
@@ -30,14 +37,28 @@ int CreateArrayLengthMin3 (string[] stringArray)
         if (stringArray[i].Length < 4)
         {
             numberOfString++;
-
         }
     }
     return numberOfString;
 }
 
+string[] CreateArrayLengthMin3(string[] stringArray, int sizeNewArray)
+{
+    string[] newStringArray = new string[sizeNewArray];
+    int j = 0;
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        if (stringArray[i].Length < 4)
+        {
+            newStringArray[j] = stringArray[i];
+            j++;
+        }
+    }
+    return newStringArray;
+}
 
-
-string[] stringArray = { "Hello", "world", "2", ":-)", "1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan" };
-PrintStringArr ( stringArray);
-WriteLine(CreateArrayLengthMin3(stringArray));
+PrintStringArr(stringArray);
+int sizeNewArray = SearchArrayLengthMin3(stringArray);
+string[] newStringArray = CreateArrayLengthMin3(stringArray, sizeNewArray);
+Write(" --> ");
+PrintStringArr(newStringArray);
